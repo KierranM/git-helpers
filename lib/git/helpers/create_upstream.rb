def49dde29fab860a17161284dcf03d48f4d08fd
Kierran McPherson <kierranm@gmail.com>
2016-04-26 07:25:26 +1200

    condition shouldn't have been negated
    Fixes #2

--- lib/git/helpers/create_upstream.rb
+++ lib/git/helpers/create_upstream.rb
@@ -11,7 +11,7 @@ module Git
     # @param print_details [Boolean] Print messages for each action or not
     def self.create_upstream(upstream_user, repo_dir = Dir.pwd, print_details = true)
       repo = Git.open(repo_dir)
-      puts "Upstream already exists" if print_details && !Utils.remote?(repo, 'upstream')
+      puts 'Upstream already exists' if print_details && Utils.remote?(repo, 'upstream')
       remote = Utils.remotes_hash(repo)['origin']
       user = Utils.true_name(remote)
       url = Utils.transform_url(remote.url, 'git')
